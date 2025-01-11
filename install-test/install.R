2adb64420d2b222bbb14d9cedc692eb6728bd594
Avinash Kulkarni <avinash.a.kulkarni@dartmouth.edu>
2025-01-11 00:06:46 -0800

    Run tests on download (warning: Tests not actually downloaded.).

--- install-test/install.R
+++ install-test/install.R
@@ -1,4 +1,11 @@
 library(devtools)
 token = Sys.getenv("GITHUB_PAT")
-devtools::install_github("a-kulkarn/qualtrics-thinkgrid", subdir="ThinkingGrid",
+devtools::install_github("a-kulkarn/qualtrics-thinkgrid",
+                         subdir="ThinkingGrid",
                          auth_token = token)
+
+## Run the tests.
+library(ThinkingGrid)
+library(testthat)
+testthat::test_package("ThinkingGrid")
+
