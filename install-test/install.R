696924a3edabe4c9c6499be0ea2fda6f092b4319
Avinash Kulkarni <avinash.a.kulkarni@dartmouth.edu>
2025-01-16 15:35:55 -0800

    Modify install test.

--- install-test/install.R
+++ install-test/install.R
@@ -1,12 +1,15 @@
 library(devtools)
-token = Sys.getenv("GITHUB_PAT")
-devtools::install_github("a-kulkarn/qualtrics-thinkgrid",
-                         subdir="ThinkingGrid",
-                         auth_token = token,
-                         INSTALL_opts = "--install-tests")
+install.packages("reticulate")
+library(reticulate)
 
-## Run the tests.
-library(ThinkingGrid)
-library(testthat)
-testthat::test_package("ThinkingGrid")
+## token = Sys.getenv("GITHUB_PAT")
+## devtools::install_github("a-kulkarn/qualtrics-thinkgrid",
+##                          subdir="ThinkingGrid",
+##                          auth_token = token,
+##                          INSTALL_opts = "--install-tests")
+
+## ## Run the tests.
+## library(ThinkingGrid)
+## library(testthat)
+## testthat::test_package("ThinkingGrid")
 
