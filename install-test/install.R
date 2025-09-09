38b5e2789f7b34d4b0b9af4117d58e21358f8bb3
Avinash Kulkarni <avinash.a.kulkarni@dartmouth.edu>
2025-09-09 16:13:02 -0700

    Update installation checker.

--- install-test/install.R
+++ install-test/install.R
@@ -1,15 +1,13 @@
-library(devtools)
-
 devtools::install_github("a-kulkarn/qualtrics-thinkgrid",
                          subdir="ThinkingGrid",
                          INSTALL_opts = "--install-tests")
 
 ## Run install.
-## library(ThinkingGrid)
 ## reticulate::install_python(version = "3.13:latest")
 ThinkingGrid::install_thinkgrid()
-library(ThinkingGrid)
+
+## Install optional dependencies for the tests.
+install.packages(c("png", "vdiffr", "lme4", "knitr", "sjPlot"))
 
 ## Run the tests.
-library(testthat)
 testthat::test_package("ThinkingGrid")
