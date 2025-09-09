42b9ea2e6dd555c10a74966f3430593cf96b8fa4
Avinash Kulkarni <avinash.a.kulkarni@dartmouth.edu>
2025-09-09 15:47:47 -0700

    Change Docker install check.

--- install-test/install.R
+++ install-test/install.R
@@ -1,11 +1,7 @@
 library(devtools)
-## install.packages("reticulate")
-## library(reticulate)
 
-token = Sys.getenv("GITHUB_PAT")
 devtools::install_github("a-kulkarn/qualtrics-thinkgrid",
                          subdir="ThinkingGrid",
-                         auth_token = token,
                          INSTALL_opts = "--install-tests")
 
 ## Run install.
